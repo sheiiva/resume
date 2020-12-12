5267fbc274e005778d0fea1c8c61a43c6ec4ef53
Corentin COUTRET-ROZET <corentin.rozet@epitech.eu>
2020-12-12 16:01:00 +0100

    [UPDATE] language switcher: current language flag auto update

--- phpapache/html/js/language.js
+++ phpapache/html/js/language.js
@@ -12,7 +12,7 @@
 var language;
 
 function getLanguage() {
-    (localStorage.getItem('language') == null) ? setLanguage('en') : false;
+    (localStorage.getItem('language') == null) ? setLanguage('us') : false;
     $.ajax({
         url:'/language/' + localStorage.getItem('language') + '.json',
         dataType: 'json', async: false,
@@ -29,5 +29,10 @@ function setLanguage(lang) {
 }
 
 function setCurrentLanguage(languages) {
+    var url = 'https://www.countryflags.io/' + localStorage.getItem('language') + '/flat/32.png';
+    var callUrl = "url(" + url + ")";
+    var styleElem = document.head.appendChild(document.createElement("style"));
+
+    styleElem.innerHTML = "#selected-lang::before {background-image: " + callUrl + ";}";
     document.getElementById('selected-lang').innerHTML = languages[localStorage.getItem('language')]
 }
